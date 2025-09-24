9401bb87ebd58e7c21dfe712da10964c4f0cb9b4
Bela Vogler <62382262+Zatyrus@users.noreply.github.com>
2025-09-24 15:00:57 +0200

    added tqdm

--- NanTex_backend/evaluation/Rhadamanthus.py
+++ NanTex_backend/evaluation/Rhadamanthus.py
@@ -6,11 +6,9 @@ import torch
 from psutil import cpu_count
 
 # eval method import
-from torcheval.metrics import PeakSignalNoiseRatio
-from pytorch_msssim import ssim, SSIM, MS_SSIM
 
 from overrides import override
-from typing import List, Dict, Any, NoReturn, Callable, Generator, Tuple
+from typing import List, Dict, Any, NoReturn, Generator, Tuple
 
 # for progress bar
 # detect jupyter notebook
@@ -19,11 +17,11 @@ from IPython import get_ipython
 try:
     ipy_str = str(type(get_ipython()))
     if "zmqshell" in ipy_str:
-        pass
+        from tqdm.notebook import tqdm
     else:
-        pass
+        from tqdm import tqdm
 except:
-    pass
+    from tqdm import tqdm
 
 # Custom Dependencies
 from ..Util.file_handler_core import FileHandlerCore
@@ -39,14 +37,15 @@ class Rhadamanthus(FileHandlerCore):
     data_path_out: str
 
     DEBUG: bool
+    disable_tqdm: bool
     metadata: Dict[str, Any]
     metrics: Dict[str, Any]
-    mode: str # has_ground_truth or no_ground_truth
+    mode: str  # has_ground_truth or no_ground_truth
     num_features: int
-    patchsize: Tuple[int, int]# default patch size
+    patchsize: Tuple[int, int]  # default patch size
 
     oneiros: Oneiros
-    device: str = "cpu" # or gpu if available
+    device: str = "cpu"  # or gpu if available
 
     def __init__(
         self,
@@ -62,7 +61,7 @@ class Rhadamanthus(FileHandlerCore):
         self.data_path_out = data_path_out
         self.data_in = {}
         self.results = {}
-        
+
         # metrics callables
         self.metrics = {}
 
@@ -75,7 +74,7 @@ class Rhadamanthus(FileHandlerCore):
 
         # internal variables
         self.metadata = {}
-        
+
         # oneiros hook
         self.oneiros = None
 
@@ -84,34 +83,46 @@ class Rhadamanthus(FileHandlerCore):
             if self.DEBUG:
                 print("Oneiros detected.")
             self.oneiros = kwargs.get("oneiros", None)
-            
+
         # Device
         if "device" in kwargs:
             if self.DEBUG:
                 print(f"Trying to set device to {kwargs.get('device')}.")
-            if kwargs.get("device") in ["gpu","cuda"]:
+            if kwargs.get("device") in ["gpu", "cuda"]:
                 import torch
+
                 if torch.cuda.is_available():
                     kwargs["device"] = "cuda"
                 else:
                     if self.DEBUG:
                         print("GPU not available, defaulting to CPU.")
                     kwargs["device"] = "cpu"
-                    
+
             if kwargs.get("device") in ["multi_cpu"]:
                 if cpu_count(logical=False) > 2:
                     self.device = "multi_cpu"
                 else:
                     if self.DEBUG:
-                        print("Multiple CPU cores not available, defaulting to single CPU.")
+                        print(
+                            "Multiple CPU cores not available, defaulting to single CPU."
+                        )
                     kwargs["device"] = "cpu"
 
         # handle patchsize
-        self.patchsize = (256, 256) # default patch size
+        self.patchsize = (256, 256)  # default patch size
         if "patchsize" in kwargs:
-            if isinstance(kwargs.get("patchsize"), (list, tuple)) and len(kwargs.get("patchsize")) == 2:
+            if (
+                isinstance(kwargs.get("patchsize"), (list, tuple))
+                and len(kwargs.get("patchsize")) == 2
+            ):
                 self.patchsize = tuple(kwargs.get("patchsize"))
-                
+
+        # handle disable_tqdm
+        self.disable_tqdm = False
+        if "disable_tqdm" in kwargs:
+            if isinstance(kwargs.get("disable_tqdm"), bool):
+                self.disable_tqdm = kwargs.get("disable_tqdm")
+
         # Call from parent class
         self.__post_init__()
 
@@ -121,7 +132,7 @@ class Rhadamanthus(FileHandlerCore):
             try:
                 self.__load_data__()
                 self.__setup_metadata__()
-                self.__arrange_data__() #<- separate feature and dream data
+                self.__arrange_data__()  # <- separate feature and dream data
 
                 if self.DEBUG:
                     print("Rhadamanthus Initialized...")
@@ -157,9 +168,7 @@ class Rhadamanthus(FileHandlerCore):
 
         ## get data paths
         files_tmp = pD.askFILES(query_title="Please select the data files")
-        data_paths_in.update(
-            {f"case_{i}": files_tmp[i] for i in range(len(files_tmp))}
-        )
+        data_paths_in.update({f"case_{i}": files_tmp[i] for i in range(len(files_tmp))})
 
         return cls(data_paths_in=data_paths_in, **kwargs)
 
@@ -208,21 +217,30 @@ class Rhadamanthus(FileHandlerCore):
 
         # run checks
         self.__run_checks__()
-        
+
         # cast to tensor
         self.__cast_to_tensor__()
-        
+
         # assure shapes
         self.__ensure_shape__()
-                
+
         # map to all data
         match self.device:
             case "cpu":
-                self.results = {key: self.__judge_cpu__(**data) for key, data in self.data_in.items()}
+                self.results = {
+                    key: self.__judge_cpu__(**data)
+                    for key, data in tqdm(self.data_in.items(), disable=self.disable_tqdm, desc="Evaluating CPU...")
+                }
             case "cuda":
-                self.results = {key: self.__judge_cuda__(**data) for key, data in self.data_in.items()}
+                self.results = {
+                    key: self.__judge_cuda__(**data)
+                    for key, data in tqdm(self.data_in.items(), disable=self.disable_tqdm, desc="Evaluating CUDA...")
+                }
             case "multi_cpu":
-                self.results = {key: self.__judge_multi_cpu__(**data) for key, data in self.data_in.items()}
+                self.results = {
+                    key: self.__judge_multi_cpu__(**data)
+                    for key, data in tqdm(self.data_in.items(), disable=self.disable_tqdm, desc="Evaluating Multi-CPU...")
+                }
             case _:
                 pass
 
@@ -234,30 +252,40 @@ class Rhadamanthus(FileHandlerCore):
                 yield method_key, method.compute().item()
             else:
                 yield method_key, method(img, pred)
-                
+
     def __judge_base__(self, features: torch.Tensor, dreams: torch.Tensor) -> Generator:
         for i in range(self.num_features):
-            yield f"feature_{i}", {k: v for k, v in self.__judgement_factory__(features[f"feature_{i}"], dreams[f"dream_{i}"])}
+            yield (
+                f"feature_{i}",
+                {
+                    k: v
+                    for k, v in self.__judgement_factory__(
+                        features[f"feature_{i}"], dreams[f"dream_{i}"]
+                    )
+                },
+            )
 
-    def __judge_cpu__(self, features:torch.Tensor, dreams:torch.Tensor) -> Dict[str, Any]:
+    def __judge_cpu__(
+        self, features: torch.Tensor, dreams: torch.Tensor
+    ) -> Dict[str, Any]:
         return dict(self.__judge_base__(features, dreams))
 
     def __judge_cuda__(self) -> NoReturn:
         raise NotImplementedError("CUDA evaluation not implemented yet.")
-    
+
     def __judge_multi_cpu__(self) -> NoReturn:
         raise NotImplementedError("Multi-CPU evaluation not implemented yet.")
 
     # %% Evaluation Metrics
     def __MSE__(self, img: np.ndarray, pred: np.ndarray) -> float:
         pass
-    
+
     def __PSNR__(self, img: np.ndarray, pred: np.ndarray) -> float:
         pass
-    
+
     def __SSIM__(self, img: np.ndarray, pred: np.ndarray) -> float:
         pass
-    
+
     def __MSSSIM__(self, img: np.ndarray, pred: np.ndarray) -> float:
         pass
 
@@ -266,45 +294,73 @@ class Rhadamanthus(FileHandlerCore):
         if self.DEBUG:
             print("Reading Dream Memory...")
         self.data_in = self.oneiros.data_out
-        
+
     def __arrange_data__(self) -> NoReturn:
         if self.DEBUG:
             print("Formatting Data...")
         for key, data in self.data_in.items():
             if isinstance(data, np.ndarray):
-                self.data_in[key] = {"features": {f"feature_{i}" : data[i] for i in range(self.num_features)}, "dreams": {f"dream_{i}": data[i+self.num_features] for i in range(0, self.num_features)}}
+                self.data_in[key] = {
+                    "features": {
+                        f"feature_{i}": data[i] for i in range(self.num_features)
+                    },
+                    "dreams": {
+                        f"dream_{i}": data[i + self.num_features]
+                        for i in range(0, self.num_features)
+                    },
+                }
             elif isinstance(data, dict):
                 if "features" in data and "dreams" in data:
                     continue
                 else:
                     raise ValueError(f"Data format for key {key} is incorrect.")
-    
+
     def __cast_to_tensor__(self) -> NoReturn:
         if self.DEBUG:
             print("Casting data to tensors...")
         for key, data in self.data_in.items():
             if isinstance(data, dict) and "features" in data and "dreams" in data:
-                if all(isinstance(v, torch.Tensor) for v in data["features"].values()) and all(isinstance(v, torch.Tensor) for v in data["dreams"].values()):
+                if all(
+                    isinstance(v, torch.Tensor) for v in data["features"].values()
+                ) and all(isinstance(v, torch.Tensor) for v in data["dreams"].values()):
                     continue
-                elif all(isinstance(v, np.ndarray) for v in data["features"].values()) and all(isinstance(v, np.ndarray) for v in data["dreams"].values()):
+                elif all(
+                    isinstance(v, np.ndarray) for v in data["features"].values()
+                ) and all(isinstance(v, np.ndarray) for v in data["dreams"].values()):
                     self.data_in[key] = {
-                        "features": {k: torch.from_numpy(v) for k, v in data["features"].items()},
-                        "dreams": {k: torch.from_numpy(v) for k, v in data["dreams"].items()},
+                        "features": {
+                            k: torch.from_numpy(v) for k, v in data["features"].items()
+                        },
+                        "dreams": {
+                            k: torch.from_numpy(v) for k, v in data["dreams"].items()
+                        },
                     }
                 else:
                     raise ValueError(f"Data format for key {key} is incorrect.")
             else:
                 raise ValueError(f"Data format for key {key} is incorrect.")
-                
+
     def __ensure_shape__(self) -> NoReturn:
         if self.DEBUG:
             print("Assuring data shapes...")
         for key, data in self.data_in.items():
             if isinstance(data, dict) and "features" in data and "dreams" in data:
-                if not any(v.shape == self.__expected_input_dim__() for v in data["features"].values()) and not any(v.shape == self.__expected_input_dim__() for v in data["dreams"].values()):
+                if not any(
+                    v.shape == self.__expected_input_dim__()
+                    for v in data["features"].values()
+                ) and not any(
+                    v.shape == self.__expected_input_dim__()
+                    for v in data["dreams"].values()
+                ):
                     self.data_in[key] = {
-                        "features": {k: v.unsqueeze(0).unsqueeze(0) for k, v in data["features"].items()},
-                        "dreams": {k: v.unsqueeze(0).unsqueeze(0) for k, v in data["dreams"].items()},
+                        "features": {
+                            k: v.unsqueeze(0).unsqueeze(0)
+                            for k, v in data["features"].items()
+                        },
+                        "dreams": {
+                            k: v.unsqueeze(0).unsqueeze(0)
+                            for k, v in data["dreams"].items()
+                        },
                     }
             else:
                 raise ValueError(f"Data format for key {key} is incorrect.")
@@ -318,7 +374,9 @@ class Rhadamanthus(FileHandlerCore):
             case "cuda":
                 for key, data in self.data_in.items():
                     self.data_in[key] = {
-                        "features": {k: v.to("cuda") for k, v in data["features"].items()},
+                        "features": {
+                            k: v.to("cuda") for k, v in data["features"].items()
+                        },
                         "dreams": {k: v.to("cuda") for k, v in data["dreams"].items()},
                     }
             case "multi_cpu":
@@ -327,7 +385,7 @@ class Rhadamanthus(FileHandlerCore):
                 pass
 
     def __expected_input_dim__(self) -> Tuple[int, int, int]:
-        return (1,1,*self.patchsize)  # (C, B, H, W)
+        return (1, 1, *self.patchsize)  # (C, B, H, W)
 
     # %% Checks
     def __run_checks__(self) -> NoReturn:
